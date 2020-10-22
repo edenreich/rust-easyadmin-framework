2940aa26f8fc3ccfa6a8a08370f158505e08dc54
Eden Reich <eden.reich@gmail.com>
2020-10-22 19:19:21 +0200

    just clone the repo rust-easyadmin when creating a new project

--- cli/main.rs
+++ cli/main.rs
@@ -1,63 +1,13 @@
 use clap::{App, Arg, ArgMatches};
-use std::fs::{create_dir, File};
-use std::io::prelude::*;
+use std::process::Command;
 
 fn create_new_project(project_name: &str) {
   println!("==> Creating new project, {}...", project_name);
-  match create_dir(format!("./{}", project_name)) {
-    Ok(p) => p,
-    Err(_error) => panic!("Problem creating the project directory, project directory already exists, delete it or give the project a different name"),
-  };
-  let mut cargo_manifest: File = match File::create(format!("{}/Cargo.toml", project_name)) {
-    Ok(file) => file,
-    Err(_error) => panic!("Problem creating the project manifest"),
-  };
-  let string: &str = "[package]
-name = \"rust-easyadmin\"
-version = \"0.1.0\"
-authors = [\"Default Name <email>\"]
-edition = \"2018\"
-
-[dependencies]
-rocket = \"0.4.5\"
-serde = { version = \"1.0\", features = [\"derive\"] }
-serde_json = \"1.0\"
-diesel = { version = \"1.3\", features = [\"sqlite\", \"r2d2\"] }
-diesel_migrations = \"1.3\"
-log = \"0.4\"
-
-[[bin]]
-name = \"bootstrap\" 
-path = \"app/bootstrap.rs\"
-";
-  match cargo_manifest.write_all(string.as_bytes()) {
-    Ok(cargo_manifest) => cargo_manifest,
-    Err(_error) => panic!("Problem writing into project manifest"),
-  };
-  match create_dir(format!("./{}/app", project_name)) {
-    Ok(p) => p,
-    Err(_error) => panic!("Problem creating app directory"),
-  };
-  let mut bootstrap: File = match File::create(format!("{}/app/bootstrap.rs", project_name)) {
-    Ok(file) => file,
-    Err(_error) => panic!("Problem creating the project manifest"),
-  };
-  let string: &str = "#[macro_use] extern crate rocket;
-
-#[get(\"/<name>/<age>\")]
-fn hello(name: String, age: u8) -> String {
-    format!(\"Hello, {} year old named {}!\", age, name)
-}
-
-#[launch]
-fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount(\"/hello\", routes![hello])
-}
-";
-  match bootstrap.write_all(string.as_bytes()) {
-    Ok(cargo_manifest) => cargo_manifest,
-    Err(_error) => panic!("Problem writing into app/bootstrap.rs"),
-  };
+  Command::new("/bin/sh")
+    .arg("-c")
+    .arg(format!("git clone https://github.com/edenreich/rust-easyadmin.git {}", project_name))
+    .output()
+    .expect("failed to execute process");
 
   // @todo add a basic project structure, something like:
   // app/
@@ -106,7 +56,7 @@ fn main() {
     .author("Eden Reich <eden.reich@gmail.com>")
     .about("An admin panel made easy written in rust.")
     .subcommand(
-      App::new("new-project").about("Create a new project").arg(
+      App::new("make:project").about("Create a new project").arg(
         Arg::new("project")
           .required(true)
           .about("The name of the project"),
@@ -141,9 +91,9 @@ fn main() {
     .get_matches();
 
   match matches.subcommand_name() {
-    Some("new-project") => create_new_project(
+    Some("make:project") => create_new_project(
       matches
-        .subcommand_matches("new-project")
+        .subcommand_matches("make:project")
         .unwrap()
         .value_of("project")
         .unwrap(),
